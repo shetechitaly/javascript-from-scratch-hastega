358d69321c4dd2316e3ba2dc8f76792f1983acd5
Chiara Corrado <chiaracorrado89@gmail.com>
2025-09-06 19:08:36 +0200

    Fix naming

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,12 +14,12 @@ const config = {
   url: 'https://shetechitaly.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/javascript-from-scratch-wl/',
+  baseUrl: '/javascript-from-scratch-hastega/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'shetechitaly', // Usually your GitHub org/user name.
-  projectName: 'javascript-from-scratch-wl', // Usually your repo name.
+  projectName: 'javascript-from-scratch-hastega', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
